ab18cb779127e0e1fe6c779b8a36e5a6f8a896a5
Lucia Paulino <lcpaulin@usc.edu>
2025-10-12 12:07:47 -0700

    fixed var

--- heap.h
+++ heap.h
@@ -189,7 +189,7 @@ void Heap<T,PComparator>::swap(int loc1, int loc2){
 template <typename T, typename PComparator>
 void Heap<T,PComparator>::print() {
 
-  for (size_t i = 0; i < heap.size() - 1; i++){
+  for (size_t i = 0; i < data.size() - 1; i++){
     std::cout << data[i] << " "; 
   }
 }
